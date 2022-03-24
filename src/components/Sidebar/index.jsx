a27f220f9e653153e5e3a21f29e1da22fa76164e
Kevin Ramos <kevinramos346@gmail.com>
2022-03-24 14:50:47 -0300

    .

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from "react";
 import Logo from "../Logo";
 import Menu from "../Menu";
 import ThemeToggle from "../ThemeToggle";
@@ -5,18 +6,46 @@ import MenuItem from "../MenuItem";
 import { menuData } from "../Menu/menuData";
 import "./index.css";
 
-const Sidebar = () => {
+const Sidebar = ({ hideable = true }) => {
+  const touchAreaRef = useRef();
+  const sidebarRef = useRef();
+
+  useEffect(() => {
+    touchAreaRef.current.addEventListener("touchmove", (event) =>
+      moveSidebar(sidebarRef.current, event)
+    );
+
+    return () =>
+      touchAreaRef.current.removeEventListener("touchmove", moveSidebar);
+  }, []);
+
   return (
-    <aside className="sidebar">
-      <Logo />
-      <Menu>
-        {menuData.map((item) => (
-          <MenuItem key={item.text} text={item.text} icon={item.icon} />
-        ))}
-      </Menu>
-      <ThemeToggle />
-    </aside>
+    <>
+      {hideable && <div className="touch-area" ref={touchAreaRef}></div>}
+      <aside className="sidebar" ref={sidebarRef}>
+        <Logo />
+        <Menu>
+          {menuData.map((item) => (
+            <MenuItem key={item.text} text={item.text} icon={item.icon} />
+          ))}
+        </Menu>
+        <ThemeToggle />
+      </aside>
+    </>
   );
+};
+
+const moveSidebar = (sidebar, event) => {
+  const style = getComputedStyle(sidebar);
+  const matrix = new DOMMatrix(style.transform);
+
+  const touchOffsetX = event.targetTouches[0].clientX;
+
+  if (matrix.m41 < 0) {
+    sidebar.style.transform = `translateX(${
+      matrix.m41 + touchOffsetX * 0.05
+    }px)`;
+  }
 };
 
 export default Sidebar;
--- src/components/ThemeProvider/theme.js
+++ src/components/ThemeProvider/theme.js
@@ -1,6 +1,6 @@
 const dark = {
   themeType: "dark",
-  primary: "#131517",
+  primary: "#3b3e40",
   secondary: "#1e1f25",
   text: "#f5fafa",
   accent: "#5e74e0"
