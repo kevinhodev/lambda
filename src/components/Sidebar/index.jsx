d93eed169d9eb9beefa46bfd1060f3b3c033d692
Kevin Ramos <kevinramos346@gmail.com>
2022-03-24 23:31:52 -0300

    .

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,44 +1,45 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import Logo from "../Logo";
 import Menu from "../Menu";
 import MenuItem from "../MenuItem";
 import ThemeToggle from "../ThemeToggle";
+import { ReactComponent as LeftArrow } from "../../assets/icons/left-arrow.svg";
 import { menuData } from "../Menu/menuData";
 import clamp from "../../utils/math";
 import "./index.css";
+import classNames from "classnames";
 
 const Sidebar = ({ hideable = true }) => {
+  const [open, setOpen] = useState(true);
   const touchAreaRef = useRef();
   const sidebarRef = useRef();
   const previousTouchOffsetX = useRef(0);
   const touchOffsetX = useRef(0);
 
   const moveSidebar = (sidebar, event) => {
-    event.preventDefault();
-
     const style = getComputedStyle(sidebar);
     const matrix = new DOMMatrix(style.transform);
 
     previousTouchOffsetX.current = touchOffsetX.current;
-    touchOffsetX.current = clamp(event.targetTouches[0]?.clientX, -70, 70);
+    touchOffsetX.current = event.targetTouches[0]?.clientX;
 
-    console.log("touchOffset: ", touchOffsetX.current, "matrix: ", matrix.m41);
+    console.log("matrix: ", matrix.m41, "touch:", touchOffsetX.current);
 
     if (
       matrix.m41 < 0 &&
       previousTouchOffsetX.current <= touchOffsetX.current
     ) {
-      sidebar.style.transform = `translateX(${
-        matrix.m41 + touchOffsetX.current * 0.05
-      }px)`;
-    } else if (
-      matrix.m41 > -67 &&
-      previousTouchOffsetX.current > touchOffsetX.current
-    ) {
-      sidebar.style.transform = `translateX(${
-        matrix.m41 - Math.abs(touchOffsetX.current) * 0.05
-      }px)`;
+      sidebar.style.transform = `translateX(${clamp(
+        matrix.m41 + touchOffsetX.current * 0.5,
+        -70,
+        0
+      )}px)`;
     }
+  };
+
+  const closeSidebar = (sidebar) => {
+    setOpen(false);
+    sidebar.style.transform = "translateX(-70px)";
   };
 
   useEffect(() => {
@@ -50,7 +51,10 @@ const Sidebar = ({ hideable = true }) => {
   return (
     <>
       {hideable && <div className="touch-area" ref={touchAreaRef}></div>}
-      <aside className="sidebar" ref={sidebarRef}>
+      <aside
+        className={classNames("sidebar", { "sidebar--close": !open })}
+        ref={sidebarRef}
+      >
         <Logo />
         <Menu>
           {menuData.map((item) => (
@@ -58,6 +62,12 @@ const Sidebar = ({ hideable = true }) => {
           ))}
         </Menu>
         <ThemeToggle />
+        <div
+          className="left-arrow"
+          onClick={() => closeSidebar(sidebarRef.current)}
+        >
+          <LeftArrow />
+        </div>
       </aside>
     </>
   );
